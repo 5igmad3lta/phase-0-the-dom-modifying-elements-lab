190e7f764cf39669aa2f7cf8fa7911a0c9e1e3d2
Stavros Droulias <stavros.d27@gmail.com>
2024-10-09 14:38:39 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+let trash = document.querySelector('main#main');
+trash.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'SUTABURO is the champion';
